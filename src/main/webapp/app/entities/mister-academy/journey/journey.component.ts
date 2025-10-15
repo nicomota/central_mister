7192c7ef80edc9a6e92c5998f95ee5c10454a8e1
Nicolas Mota <nicolas.mota2000@gmail.com>
2025-10-15 10:38:26 -0300

    ajuster progresso da trilha journey

--- src/main/webapp/app/entities/mister-academy/journey/journey.component.ts
+++ src/main/webapp/app/entities/mister-academy/journey/journey.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./journey.component.scss']
 })
 export class JourneyComponent implements OnInit {
+  // Controla quais módulos estão expandidos
+  expandedModules: { [key: number]: boolean } = {
+    1: true,  // Módulo 1 expandido por padrão
+    2: true,  // Módulo 2 expandido por padrão
+    3: false,
+    4: false
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  toggleModule(moduleNumber: number): void {
+    this.expandedModules[moduleNumber] = !this.expandedModules[moduleNumber];
+  }
+
+  isModuleExpanded(moduleNumber: number): boolean {
+    return this.expandedModules[moduleNumber];
+  }
 }
